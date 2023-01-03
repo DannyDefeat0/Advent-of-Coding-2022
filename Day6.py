371719034c8871b1c25e0576d6a425c90aa1e130
Damon <damonmelton@gmail.com>
2023-01-03 11:41:20 -0800

    All

--- Day6.py
+++ Day6.py
@@ -3,9 +3,7 @@ lines = file.readlines()
 
 for line in lines:
     for i in range(len(line)-13):
-        check = [line[k] for k in range(i+1, i+14)]
-        if line[i] in check or len(check) != len(set(check)):
-            pass
-        else:
+        check = [line[k] for k in range(i, i+14)]
+        if len(check) == len(set(check)):
             print(i+14)
             break
