7a89f573962878acf880925f796dbf9773b8399b
Damon <damonmelton@gmail.com>
2023-03-21 14:06:33 -0700

    All

--- Day9-2.py
+++ Day9-2.py
@@ -3,15 +3,43 @@ lines = file.readlines()
 
 
 checks = [2**0.5, 0, 1]
-
+position = [[0,0] for i in range(10)]
 s = [0, 0]
-tail_set = {tuple(s)}
+tail_set = {tuple(position[9])}
 
-tail = [0,0]
-head = [0,0]
+def subtract(tuple1, tuple2):
+    dif = tuple(map(lambda i, j: i - j, tuple1, tuple2))
+    return dif
+def add(tuple1,tuple2):
+    summation = tuple(map(lambda i, j: i + j, tuple1, tuple2))
+    return summation
 
-position = [head, tail]
+position = [[0,0] for i in range(10)]
 
+
+def move_tail(head_position, tail_position):
+    gap = subtract(head_position, tail_position)
+    dist_lookup={
+        (2, 2): (1, 1),
+        (2, 1): (1, 1),
+        (2, 0): (1, 0),
+        (2, -1): (1, -1),
+        (2, -2): (1, -1),
+        (1, 2): (1, 1),
+        (1, -2): (1, -1),
+        (0, 2): (0, 1),
+        (0, -2): (0, -1),
+        (-1, 2): (-1, 1),
+        (-1, -2): (-1,-1),
+        (-2, -1): (-1, -1),
+        (-2, 2): (-1, 1),
+        (-2, 0): (-1, 0),
+        (-2, 1): (-1, 1),
+        (-2, -2): (-1, -1),
+      }
+    if gap in dist_lookup:
+        tail_position = add(tail_position, dist_lookup[gap])
+    return tail_position
 def dist(pos1, pos2):
     euclidean_distance = (((pos2[0]-pos1[0])**2)+((pos2[1]-pos1[1])**2))**0.5
     return euclidean_distance
@@ -23,75 +51,29 @@ def dist(pos1, pos2):
 # (-1,-1) (0, -1) (1, -1)
 
 
-def move(head_position, tail_position, direction, num_to_move):
+def move_head(head_position, direction):
     if direction == "R":
-        end = head_position[0] + num_to_move
-        while head_position[0] != end:
-            head_position[0] += 1
-            check = dist(head_position, tail_position)
-            if check in checks:
-                continue
-            else:
-                tail_position[0] += 1
-                check = dist(head_position, tail_position)
-                if head_position[1] - tail_position[1] > 0:
-                    tail_position[1] += 1
-                elif head_position[1] - tail_position[1] < 0:
-                    tail_position[1] -= 1
-            tail_set.add(tuple(tail_position))
+        head_position[0] += 1
     elif direction == "L":
-        end = head_position[0] - num_to_move
-        while head_position[0] != end:
-            head_position[0] -= 1
-            check = dist(head_position, tail_position)
-            if check in checks:
-                continue
-            else:
-                tail_position[0] -= 1
-                check = dist(head_position, tail_position)
-                if head_position[1] - tail_position[1] > 0:
-                    tail_position[1] += 1
-                if head_position[1] - tail_position[1] < 0:
-                    tail_position[1] -= 1
-            tail_set.add(tuple(tail_position))
+        head_position[0] -= 1
     elif direction == "U":
-        end = head_position[1] + num_to_move
-        while head_position[1] != end:
-            head_position[1] += 1
-            check = dist(head_position, tail_position)
-            if check in checks:
-                continue
-            else:
-                tail_position[1] += 1
-                check = dist(head_position, tail_position)
-                if head_position[0] - tail_position[0] > 0:
-                    tail_position[0] += 1
-                elif head_position[0] - tail_position[0] < 0:
-                    tail_position[0] -= 1
-            tail_set.add(tuple(tail_position))
+        head_position[1] += 1
     elif direction == "D":
-        end = head_position[1] - num_to_move
-        while head_position[1] != end:
-            head_position[1] -= 1
-            check = dist(head_position, tail_position)
-            if check in checks:
-                continue
-            else:
-                tail_position[1] -= 1
-                check = dist(head_position, tail_position)
-                if head_position[0] - tail_position[0] > 0:
-                    tail_position[0] += 1
-                elif head_position[0] - tail_position[0] < 0:
-                    tail_position[0] -= 1
-            tail_set.add(tuple(tail_position))
-    return [head_position, tail_position]
-
+        head_position[1] -= 1
+    return head_position
+def move(head_position, direction, num_to_move):
+    while num_to_move > 0:
+        position[0] = move_head(head_position, direction)
+        num_to_move -= 1
+        for i in range(1, len(position)):
+            position[i] = move_tail(position[i-1], position[i])
+        tail_set.add(tuple(position[9]))
+    return position
 for line in lines:
     input = line.split()
     ref = input[0]
     num = int(input[-1])
-    position = move(position[0], position[1], ref, num)
-    print(position)
-    #FIRST MOVE IS GETTING COUNTED FUCK YOU GAME
+    position = move(position[0], ref, num)
+    #print(position)
 print(tail_set)
 print(len(tail_set))
